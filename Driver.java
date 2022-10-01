3ed29cc6f996dbac3217964a4a825a6e4a43d657
Pieter <pieter.talens@gmail.com>
2022-10-01 01:20:43 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,78 @@
+import java.io.*;
+import java.io.IOException;
+
 public class Driver {
     public static void main(String [] args) {
-    Polynomial p = new Polynomial();
-    System.out.println(p.evaluate(3));
-    double [] c1 = {6,0,0,5};
-    Polynomial p1 = new Polynomial(c1);
-    double [] c2 = {0,-2,0,0,-9};
-    Polynomial p2 = new Polynomial(c2);
-    Polynomial s = p1.add(p2);
-    System.out.println("s(0.1) = " + s.evaluate(0.1));
-    if(s.hasRoot(1))
-        System.out.println("1 is a root of s");
-    else
-        System.out.println("1 is not a root of s");
+
+	System.out.println("\n\n");
+        double [] c1 = {6,0,0,5};
+        int[] e1 = {1,2,4,3};
+        Polynomial p1 = new Polynomial(c1, e1);
+        //p1.print();
+
+        double [] c2 = {1,-2,0,-9};
+        int[] e2 = {1,2,3,7};
+        Polynomial p2 = new Polynomial(c2, e2);
+	    //p2.print();
+
+        double [] c3 = {1,2,0,4,5,2};
+        int [] e3 = {3,4,5,5,5,5};
+        Polynomial p3 = new Polynomial(c3, e3);
+        //p3.print();
+
+        double [] c4 = {1};
+        int [] e4 = {2};
+        Polynomial p4 = new Polynomial(c4, e4);
+
+        double [] c5 = {1, 2};
+        int [] e5 = {1, 2};
+        Polynomial p5 = new Polynomial(c5, e5);
+
+        double [] c6 = {4, 2};
+        int [] e6 = {0, 1};
+        Polynomial p6 = new Polynomial(c6, e6);
+
+        Polynomial sum1 = p4.add(p5);
+        System.out.println("Added " + p4.asString());
+        System.out.println("and "+ p5.asString());
+        System.out.println("and got: "+sum1.asString());
+        System.out.println();
+
+        Polynomial sum2 = p1.add(p2);
+        System.out.println("Added " + p1.asString());
+        System.out.println("and "+ p2.asString());
+        System.out.println("and got: "+sum2.asString());
+        System.out.println();
+        
+        Polynomial prod1 = p3.multiply(p5);
+        System.out.println("Multiplied: "+p3.asString());
+        System.out.println("With: "+p5.asString());
+        System.out.println("and got: "+prod1.asString());
+        System.out.println();
+        
+        Polynomial prod2 = p2.multiply(p4);
+        System.out.println("Multiplied: "+p2.asString());
+        System.out.println("With: "+p4.asString());
+        System.out.println("and got: "+prod2.asString());
+        System.out.println();
+        
+        Polynomial sum3 = prod2.add(p6);
+        System.out.println("Added " + p1.asString());
+        System.out.println("and "+ p2.asString());
+        System.out.println("and got: "+sum2.asString());
+        System.out.println();
+
+		File directory = new File("./");
+		String path = directory.getAbsolutePath();
+        path = path.substring(0,path.length()-1)+"testOutput01.txt";
+        p6.saveToFile(path);
+        System.out.println("Wrote: "+p6.asString()+" to file: "+path);
+
+        File p6File = new File(path);
+        Polynomial p7 = new Polynomial(p6File);
+        System.out.println("Read from file: "+p7.asString());
+        p6File.delete();
+        System.out.println("File deleted. Please check around line 74 of Driver.java inspect code and or comment out if you want to inspect the file.");
+
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,39 +1,239 @@
+//lab 2 version
 import java.lang.Math;
+import java.io.*;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Polynomial{
     double[] coefficients;
+    int[] exponents;
 
     public Polynomial(){
         this.coefficients = new double[0];
+        this.exponents = new int[0];
+    }
+    
+    public Polynomial(double[] newCoefs, int[] newExpos){
+        this.coefficients = newCoefs;
+        this.exponents = newExpos;
     }
 
-    public Polynomial(double[] newCoefs){
-        this.coefficients = newCoefs;
+    public Polynomial(File inputFile){        
+        try{
+            Scanner scan = new Scanner(inputFile);
+            String inputText = scan.nextLine();
+            scan.close();
+            double[] tempCoefs = new double[inputText.length()];
+            int[] tempExpos = new int[inputText.length()];            
+            int[] signs = getSigns(inputText);            
+            String[] terms = inputText.split("[-+]");               
+            for (int i =0; i<terms.length; i++){
+                int xIndex = terms[i].indexOf('x');
+                if (xIndex == -1){
+                    double coef = Double.valueOf(terms[i].substring(0));   
+                    if(signs[i] == 0)       
+                        coef=(-1.0)*coef; 
+                    tempCoefs[i] = coef;
+                    tempExpos[i] = 0;   
+                }
+                else{
+                    double coef = Double.valueOf(terms[i].substring(0,xIndex));     
+                    if(signs[i] == 0)       
+                        coef=(-1.0)*coef;
+                    if(terms[i].substring(xIndex+1) == ""){
+                        tempExpos[i] = 1;   
+                    }
+                    else{
+                        int expo = Integer.valueOf(terms[i].substring(xIndex+1));
+                        tempExpos[i] = expo;   
+                    }
+                    tempCoefs[i] = coef;         
+                }
+            }
+            int length = countNonZeroCoefs(tempCoefs);
+            double[] newCoefs = new double[length];
+            int[] newExpos = new int[length];
+            int index = 0;
+            for (int i =0; i< tempExpos.length; i++){
+                if(tempCoefs[i] != 0 && tempCoefs[i] != 0.0){
+                    newCoefs[index] = tempCoefs[i];
+                    newExpos[index] = tempExpos[i];
+                    index = index +1;
+                }
+            }
+            this.coefficients = newCoefs;
+            this.exponents = newExpos;
+        } catch(IOException e){
+            System.out.println("Exception encountered loading file");
+            e.printStackTrace();
+        }
+    }
+
+    protected int[] getSigns(String inputText){
+        int[] series = new int[inputText.length()-3];
+        int index = 1;
+        series[0] = 1;
+        for (int i = 0; i<inputText.length()-3;i++){
+            if (inputText.charAt(i) == '-'){
+                series[index] = 0;
+                index = index+1;
+            }
+            if (inputText.charAt(i) == '+'){
+                series[index] = 1;
+                index = index+1;
+            }
+        }
+        return series;
+    }
+    public void saveToFile(String fileName){
+        String output = "";
+        for (int i = 0; i < this.exponents.length; i ++){
+            if(this.coefficients[i] != 0)
+                if (this.exponents[i] == 0){
+                    output = (String)(output+this.coefficients[i]);
+                }
+                else if(this.exponents[i] == 1){
+                    output = (String)(output+this.coefficients[i]+"x");
+                }
+                else{
+                output = (String)(output+this.coefficients[i]+"x"+this.exponents[i]);
+                }
+            if(i+1 < this.exponents.length){
+                if(this.coefficients[i+1] > 0){
+                    output = (String)(output+"+");
+                }
+            }
+        }
+
+        try{
+            FileWriter writer = new FileWriter((String)fileName);            
+            writer.write(output);
+            writer.close();
+        } catch(IOException e){
+            System.out.println("Exception encountered while saving to file");
+            e.printStackTrace();
+        }
     }
 
     public Polynomial add(Polynomial other){
-        int thisLength = this.coefficients.length;
-        int otherLength = other.coefficients.length;
-        int newCoefsLength = Math.max(thisLength, otherLength);
-        double[] newCoefs = new double[newCoefsLength];
+        int largest = getLargest(this.exponents, other.exponents);
+        int smallest = getSmallest(this.exponents, other.exponents);
+        int totalExps =  largest - smallest +1;     
+        double[] tempCoefs = new double[totalExps];
+        int[] tempExpos = new int[totalExps];
+        for (int i = smallest; i < largest +1; i ++){
+        	tempExpos[i] = i;
+        }        
+        for (int i = 0; i < this.exponents.length; i ++){
+		    int exp = this.exponents[i];
+        	tempCoefs[exp] = tempCoefs[exp] + this.coefficients[i]; 
+        }
+        for (int i = 0; i < other.exponents.length; i ++){
+		    int exp = other.exponents[i];
+        	tempCoefs[exp] = tempCoefs[exp] + other.coefficients[i]; 
+        } 
+        int newLength = countNonZeroCoefs(tempCoefs);
+        double[] newCoefs = new double[newLength];
+        int[] newExpos = new int[newLength];        
+        int newIndex = 0;
+        for (int i =0; i <tempExpos.length; i++){
+            if (tempCoefs[i] != 0 || tempCoefs[i] != 0.0){
+                newCoefs[newIndex] = tempCoefs[i];
+                newExpos[newIndex] = tempExpos[i];
+                newIndex = newIndex+1;
+            }
+        }
+        return new Polynomial(newCoefs, newExpos);
+    }
 
-        for (int i = 0; i < thisLength; i++){
-            newCoefs[i] = this.coefficients[i];
+    public Polynomial multiply(Polynomial other){
+        int maxLength = this.exponents.length * other.exponents.length;
+        double[] newCoefs = new double[maxLength];
+        int[] newExpos = new int[maxLength];
+        for (int i = 0; i < this.exponents.length; i++){
+            for (int j = 0; j < other.exponents.length; j++){
+                newExpos[i+j] = this.exponents[i]+other.exponents[j];
+                newCoefs[i+j] = this.coefficients[i]*other.coefficients[j];
+            }
         }
-        for (int i = 0; i < otherLength; i++){
-            newCoefs[i] = newCoefs[i] + other.coefficients[i];
-        }
-        return new Polynomial(newCoefs);
+        double[]c = {0};
+        int[]e = {0};
+        Polynomial dumbPoly = new Polynomial(c,e);
+        Polynomial newPoly = new Polynomial(newCoefs, newExpos);
+        return newPoly.add(dumbPoly);
     }
 
     public double evaluate(double value){
         double result = 0.0;
         for(int i =0; i < this.coefficients.length; i ++){
-              result = result + this.coefficients[i]*(Math.pow(value, i));
+              result = result + this.coefficients[i]*(Math.pow(value, exponents[i]));
         }
         return result;
     }
 
     public boolean hasRoot(double value){
         return this.evaluate(value) == 0.0;
+    }
+
+    protected int getLargest(int[] arr1, int[] arr2){
+        int largest = 0;
+        for (int num: arr1)
+            if (num > largest) largest = num;
+        for (int num: arr2)
+            if (num > largest) largest = num;
+        return largest;
+    }
+    
+    protected int getSmallest(int[] arr1, int[] arr2){
+        int smallest = 0;
+        for (int num: arr1)
+            if (num < smallest) smallest = num;
+        for (int num: arr2)
+            if (num < smallest) smallest = num;
+        return smallest;
+    }
+
+    protected int countNonZeroCoefs(double[] coefs){
+        int nonZeros = 0;
+        for (int i = 0; i<coefs.length; i++){
+            if(coefs[i] != 0.0){
+                nonZeros = nonZeros+1;
+            }
+        }
+        return nonZeros;
+    }
+
+    protected void print(){
+        String output = asString();
+        System.out.println(output);
+    }
+
+    protected String asString(){
+        String output = "";
+        for (int i = 0; i< coefficients.length; i++){
+            output = output +" "+coefficients[i]+"x^("+exponents[i]+")";
+        }
+        return output;
+    }
+
+    protected void oneFactorMultiply(double coefFactor, int coefExp, double[] coefArray, int[] expoArray){
+        for (int i = 0; i<coefArray.length; i++){
+            coefArray[i] = coefArray[i] * coefFactor;
+            expoArray[i] = expoArray[i] + coefExp;
+        }
+    }
+
+    protected int[] copyArray(int[] arrayToCopy){
+        int[] newArray = new int[arrayToCopy.length];
+        for (int i = 0; i < arrayToCopy.length; i++)
+            newArray[i] = arrayToCopy[i];
+        return newArray;
+    }
+
+    protected double[] copyArray(double[] arrayToCopy){
+        double[] newArray = new double[arrayToCopy.length];
+        for (int i = 0; i < arrayToCopy.length; i++)
+            newArray[i] = arrayToCopy[i];
+        return newArray;
     }
 }
